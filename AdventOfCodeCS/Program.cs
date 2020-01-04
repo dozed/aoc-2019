e20d8881213d7062950feb4a686d9398b5b3a710
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2020-01-04 15:38:21 +0100

    Added exercise 16

---
+++ AdventOfCodeCS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCodeCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Exercise16.part1();
+
+        }
+    }
+}
